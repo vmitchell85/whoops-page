da888c17aebc4c6793ae88c7cacc4ec56a1e02a8
Vince Mitchell <vincent.mitchell@gmail.com>
2018-02-17 11:22:58 -0500

    add php version

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <link href='http://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                color: #CCC;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+                background-color: #333;
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 60px;
+                margin-bottom: 40px;
+            }
+
+            p {
+                font-size: 24px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Whoops</div>
+                <p>
+                    We couldn't find that site... please check your URL and try again!
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
+
